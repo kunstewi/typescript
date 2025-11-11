7eacd3d30823031ea4dccb08da08d3942b4f9c68
kunstewi <kan.ai35423@gmail.com>
2025-11-11 21:51:46 +0530

    added checklist basics

--- 01.basics/01.inference.ts
+++ 01.basics/01.inference.ts
@@ -4,11 +4,11 @@
 // 👉 Goal: Observe when TypeScript can infer types and when you need explicit annotations.
 
 // typescript returns the infer type number as a and b are explicitly number
-function add(a: number, b: number) {
+function add4(a: number, b: number) {
   return a + b;
 }
 
-const sum = add(2, 3);
+const sum = add4(2, 3);
 
 
 // without explicit parameter types it gives an error as p and q are of type any
