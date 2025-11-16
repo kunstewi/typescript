3260a9642419c5e1f605abd83f6bcc7611552d72
kunstewi <kan.ai35423@gmail.com>
2025-11-16 13:25:52 +0530

    conditionals revision done

--- 03.conditionals/08.switch.ts
+++ 03.conditionals/08.switch.ts
@@ -7,3 +7,42 @@
 // "Large" for 3
 
 // But use fall-through so that 1 and 2 both print "Small or Medium".
+
+const scnum: number = 3;
+
+switch (scnum) {
+  case 1:
+    console.log("Small");
+    break;
+  case 2:
+    console.log("Medium");
+    break;
+  case 3:
+    console.log("Large");
+    break;
+  default:
+    console.log("Invalid number");
+    break;
+}
+
+
+// fall through version 
+
+/*
+const scnum: number = 3;
+
+switch (scnum) {
+  case 1:
+  case 2:
+    console.log("Small or Medium");
+    break;
+
+  case 3:
+    console.log("Large");
+    break;
+
+  default:
+    console.log("Invalid number");
+    break;
+}
+*/
--- 03.conditionals/10.type-condition.ts
+++ 03.conditionals/10.type-condition.ts
@@ -9,3 +9,28 @@
 // "Unknown type" otherwise
 
 // Use both if-else and a switch version for practice.
+
+function describeInput(x: any) {
+  if (typeof x === "string") {
+    return "String";
+  } else if (typeof x === "number") {
+    return "Number";
+  } else if (typeof x === "boolean") {
+    return "Boolean";
+  } else {
+    return "Unknown Type";
+  }
+}
+
+function describeInputSwitch(x: any) {
+  switch (typeof x) {
+    case "string":
+      return "String";
+    case "boolean":
+      return "Boolean";
+    case "number":
+      return "Number";
+    default:
+      return "Unknwon Type";
+  }
+}
