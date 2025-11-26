dbff00637b37e46584984f86ae62b438bb9096a9
kunstewi <kan.ai35423@gmail.com>
2025-11-26 10:58:19 +0530

    basics doen

--- 04.loops/01.arrayLooping.ts
+++ 04.loops/01.arrayLooping.ts
@@ -2,7 +2,7 @@
 
 const fruits: String[] = ["apple", "banana", "cherry", "mango"];
 
-// Use a for...of loop to print each fruit in uppercase.
+// Use a for...of loop to print each fruit in uppercase. also use and for loop
 
 for (let i: number = 0; i < fruits.length; i++) {
   console.log(fruits[i].toUpperCase());
